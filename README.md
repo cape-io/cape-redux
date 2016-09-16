14504e6cb3d8bd9d1ee794c7e111681976b766e8
Kai Curry <kai@cape.io>
2016-09-16 13:04:49 -0500

    readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # cape-redux v1.0.0
+
+- `addListener(selector, onChange, store)` - Trigger a call to onChange() when result of selector changes.
+- `createAction(type, payloadCreator)` - Makes an action creator.
+- `createReducer(reducers, defaultState = {})` - Send reducers obj where key is type and value is func with (state, payload) sig.
+- `mapDispatchToProps(getActions)` - getActions func is passed state. Result is passed to bindActionCreators.
+- `thunkAction` - Like createSelector but it builds and dispatches an action creator.
--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import { bindActionCreators } from 'redux'
 import { isError, isFunction, identity, isObject, isString, isUndefined, pickBy } from 'lodash'
 
-// Like createSelector but it builds and dispatches an action creator.
-export function thunkAction(...funcs) {
-  const action = funcs.pop()
-  return (props, ...args) => (dispatch, getState) => {
-    const params = funcs.map(dependency => dependency(getState(), props, ...args))
-    dispatch(action(...params))
-  }
-}
-export function mapDispatchToProps(getActions) {
-  return (dispatch, props) => bindActionCreators(getActions(props), dispatch)
-}
-
 // Trigger a call to onChange() when result of selector changes.
 export function addListener(selector, onChange, store) {
   const { getState, subscribe } = store
@@ -26,6 +14,7 @@ export function addListener(selector, onChange, store) {
   }
   return subscribe(handleChange)
 }
+// @TODO Make this more functional style.
 export function createAction(type, payloadCreator) {
   const getPayload = isFunction(payloadCreator) ? payloadCreator : identity
   return function actionCreator(arg1, arg2, arg3) {
@@ -50,10 +39,23 @@ export function createAction(type, payloadCreator) {
     return action
   }
 }
+// Send reducers obj where key is type and value is func with (state, payload) sig.
 export function createReducer(reducers, defaultState = {}) {
   return function reducer(state = defaultState, action) {
     if (action.error || !action.type || !isFunction(reducers[action.type])) return state
     if (!action.payload) return state
     return reducers[action.type](state, action.payload)
+  }
+}
+// getActions() is passed state. Result is passed to bindActionCreators.
+export function mapDispatchToProps(getActions) {
+  return (dispatch, props) => bindActionCreators(getActions(props), dispatch)
+}
+// Like createSelector but it builds and dispatches an action creator.
+export function thunkAction(...funcs) {
+  const action = funcs.pop()
+  return (props, ...args) => (dispatch, getState) => {
+    const params = funcs.map(dependency => dependency(getState(), props, ...args))
+    dispatch(action(...params))
   }
 }
